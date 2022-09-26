eeca778e1925fb3b1e082af74dd16a953317f420
p011uckz <mrbendy88@gmail.com>
2022-09-26 19:45:51 -0400

    more stuff0926

--- portfront/src/App.js
+++ portfront/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={ <Home /> } />
+        <Route path='/projects' element={ <Projects /> } />
       </Routes>
     </div>
   );
--- portfront/src/pages/Projects.jsx
+++ portfront/src/pages/Projects.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const Homediv = styled.div`
+const Projdiv = styled.div`
 background-image: url(https://wallpaperaccess.com/full/1271508.jpg);
-/* background-image: url(https://images.pling.com/img/00/00/64/96/96/1738390/neon-221.jpg); */
 background-size: 1920px, 1080px;
 background-repeat: no-repeat;
 min-width: 100%;
@@ -12,18 +11,12 @@ min-height: 100%;
 position: fixed;
 color: aquamarine;
 
-
-.yellow{
+h1{
     text-align: center;
-    margin-right: 30px;
 }
-.red{
-    text-align: center;
-    color:aquamarine;
-    margin-right: 30px;
-    text-align: 
-};
-
+p{
+    float: right;
+}
 h1 {
     text-align: center;
     -webkit-animation: color-change 5s infinite;
@@ -68,72 +61,70 @@ h1 {
     75% { color: yellow; }
     100% { color: aquamarine; }
 }
-
-footer{
-    text-align: right;
-    margin-right: 30px;
-    color: aquamarine
-}
-h3{
-    /* vertical-align: text-bottom; */
-    text-align-last: right;
-    margin-right: 30px;
+.home{
+    /* float: left; */
+    align-items: right;
+    font-size: 20px;
     color: aquamarine;
 }
-a{
-    color: green;
+a:visited{
+    text-decoration: none;
+    decoration: none;
 }
 button{
     background-color: Transparent;
     background-repeat:no-repeat;
     cursor:pointer;
     color: aquamarine;
-    font-size: 20px;
-    /* align-items: right; */
+
+    /* border-radius: 28px;
+    background-image: url(https://wallpaperaccess.com/full/1271508.jpg);
+    background-repeat: no-repeat;
+    background-size: 1000px;
+    position: fixed; */
+
+    /* border: none; */
+    /* overflow: hidden; */
+
+    /* background-image: url(https://cdn.xxl.thumbs.canstockphoto.com/blue-button-one-blue-neon-button-empty-black-background-drawings_csp1114649.jpg); */
+    /* min-width: 10%;
+    min-height: 10%; */
+    /* background-position: center; */
 }
 `
 
-
-const Home = () => {
+const Projects = () => {
   return (
-    <Homediv>
-        <div>
-            <h1>Hi there, My name is Jeremy</h1><h1> Welcome to my website</h1>
-            <h5 className='yellow'>I am a software developer</h5>
-            <h4 className='red'>I can use many programming languages<br></br>
-            and tools like:<br></br>
-            HTML<br></br>
-            CSS<br></br>
-            Python<br></br>
-            SQL<br></br>
-            Django<br></br>
-            JavaScript<br></br>
-            Express<br></br>
-            Json<br></br>
-            MongoDB<br></br>
-
-            </h4>
-        </div>
-        <footer>
-            
-            
-            
+    <Projdiv>
+        <h1>My Projects</h1>
+        <header>
+            <Link to='/'><button className='home'>
+                 <p> Home </p> 
+            </button></Link>
+            <p>Here is where I will have links to my projects, once I have it all working.</p>
             <br></br>
             <br></br>
+            <p>
+                <a href=""><button>Blackjack</button></a>
+            </p>
             <br></br>
             <br></br>
+            <p>
+                <a href=""><button>Movie List</button></a>
+            </p>
             <br></br>
             <br></br>
+            <p>
+                <a href='https://marvel-wiki-project3.herokuapp.com/'><button>Marvel Wiki</button></a>
+            </p>
             <br></br>
             <br></br>
-            <h3> <Link to='/projects'><button>Projects</button></Link> </h3>
-            <h3> <a href={"https://github.com/p011uckz/"}><button>Here is my Github: </button></a> </h3>
-            <h3> <a href={"https://www.linkedin.com/in/jeremy-currier/"}><button>Here is my Linkedin:</button></a></h3>
-
-            
-            </footer>
-    </Homediv>
+            <p>
+                <a href=''><button>Chacracter Creator Plus</button></a>
+            </p>
+        </header>
+    </Projdiv>
   )
 }
 
-export default Home
+export default Projects
